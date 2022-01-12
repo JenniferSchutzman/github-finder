b34cad71394f2ebc4353f24d11af52c26cf2e0cc
Jenny Schutzman <jenny.schutzman@ratequote.com>
2022-01-12 09:01:53 -0500

    daisyui and tailwind config.  Components/layout/navbar start

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,9 @@
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+function Navbar({ title }) {
+  return <div></div>;
+}
+
+export default Navbar;
